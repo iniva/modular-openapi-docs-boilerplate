011aa0ba4b52cc6a55e38a4435d5c3d767de82e5
Tony <tonyblast@gmail.com>
2018-05-10 21:53:58 -0300

    Added example of modular docs with yaml files

---
+++ docs/builder.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const path = require('path');
+const refs = require('refs');
+const debug = require('debug');
+
+const logger = debug('docs:builder');
+const inputTemplate = path.resolve(`${__dirname}/spec.yaml`);
+const outputFile = path.resolve(`${__dirname}/openapi.yaml`);
+
+logger('Building documentation');
+try {
+    refs(inputTemplate, outputFile)
+        .then(results => {
+            logger(`Documentation created in ${results.outputFile}`);
+        })
+        .catch(error => {
+            logger(`An error occurred while building the documentation: ${error.message}`);
+            logger(error.stack);
+        });
+}
+catch (error) {
+    logger(`An error occurred while building the documentation: ${error.message}`);
+    logger(error.stack);
+}
+
+process.on('unhandledRejection', error => {
+    logger(`An Unhandled Rejection happened: ${error.message}`);
+    logger(error.stack);
+});
---
+++ index.js
@@ -0,0 +1,1 @@
+// Your awesome API entry point
